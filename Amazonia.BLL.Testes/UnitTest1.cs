37bc02e8068a80b8a5ff6b6332ad2cfd6351ca0c
Nuno Santos <nunojmdsantos@gmail.com>
2021-10-26 21:47:24 +0100

    testes automatizados

---
+++ Amazonia.BLL.Testes/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Amazonia.BLL.Testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
