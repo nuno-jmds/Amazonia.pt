8c5d7d442cd4082e9f419ef79494b68606838d62
Nuno Santos <nunojmdsantos@gmail.com>
2021-10-28 23:23:36 +0100

    testes cliente

---
+++ Amazonia.DAL.Tests/Entidades/ClienteTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Amazonia.DAL.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazonia.DAL.Entidades.Tests
+{
+    [TestClass()]
+    public class ClienteTests
+    {
+        [TestMethod()]
+        public void NifEstaValidoTest()
+        {
+
+            //Arrange
+            var cliente = new Cliente();
+            cliente.NumeroIdentificacaoFiscal = "248129554";
+            //Act
+            var nifValido = cliente.NifEstaValido();
+            //Assert
+            Assert.IsTrue(nifValido);
+        }
+
+        [TestMethod()]
+        public void DeveInvalidarNifMaiorQue9Digitos ()
+        {
+
+            //Arrange
+            var cliente = new Cliente();
+            cliente.NumeroIdentificacaoFiscal = "2481295542";
+            //Act
+            var nifInValido = !cliente.NifEstaValido();
+            //Assert
+            Assert.IsTrue(nifInValido);
+        }
+        [TestMethod()]
+        public void DeveInvalidarNifMenorQue9Digitos()
+        {
+
+            //Arrange
+            var cliente = new Cliente();
+            cliente.NumeroIdentificacaoFiscal = "24812955";
+            //Act
+            var nifInValido = !cliente.NifEstaValido();
+            //Assert
+            Assert.IsTrue(nifInValido);
+        }
+
+        [TestMethod()]
+        public void DeveInvalidarNifNumerosIguais()
+        {
+
+            //Arrange
+            var cliente = new Cliente();
+            cliente.NumeroIdentificacaoFiscal = "111111111";
+            //Act
+            var nifInValido = !cliente.NifEstaValido();
+            //Assert
+            Assert.IsTrue(nifInValido);
+        }
+    }
+}
--- Amazonia.DAL/Entidades/Cliente.cs
+++ Amazonia.DAL/Entidades/Cliente.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Amazonia.DAL.Entidades
 {
@@ -12,6 +13,44 @@ namespace Amazonia.DAL.Entidades
         public DateTime DataNascimento { get; set; }
         public string NumeroContribuinte { get; set; }
         public int Idade => DateTime.Now.Year - DataNascimento.Year;
+
+        public string NumeroIdentificacaoFiscal { get; set; }
+
+        public bool NifEstaValido()
+        {
+            if(this.NumeroIdentificacaoFiscal.Length!=9)
+                return false;
+
+            if (NumeroIdentificacaoFiscal.ToCharArray().Distinct().ToList().Count == 1)
+                return false;
+
+            var produtoSomatorio = 0;
+            var fatorMultiplicacao= 2;
+            for (int i = NumeroIdentificacaoFiscal.Length - 2; i >= 0; i--)
+            {
+                var elemento = (int)Char.GetNumericValue(NumeroIdentificacaoFiscal[i]);
+                var produto = elemento * fatorMultiplicacao;
+                produtoSomatorio += produto;
+                fatorMultiplicacao++;
+                
+
+            }
+            var restoDivisaoPor11 = produtoSomatorio % 11;
+            if ((restoDivisaoPor11==0||restoDivisaoPor11==1)&& (int)Char.GetNumericValue(NumeroIdentificacaoFiscal[8]) == 0)
+            {
+                return true;
+            }
+            else 
+            {
+                int ultimo= (int)Char.GetNumericValue(NumeroIdentificacaoFiscal[8]);
+                return ((11 - restoDivisaoPor11) == (ultimo));
+            }
+            
+
+            
+        }
+
+
 
         public override string ToString()
         {
